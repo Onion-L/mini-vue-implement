101afd437632c43eed3662468d45e59283bd4dc4
Xiang Li <lixiang020317@gmail.com>
2024-05-26 11:24:12 +0800

    Runtime: Initialise the component main process

---
+++ src/runtime-core/createApp.ts
@@ -0,0 +1,16 @@
+import { render } from "./renderer";
+import { createVNode } from "./vnode";
+
+export function createApp(rootComponet) {
+    return {
+        mount(rootContainer) {
+            /**
+             * 一般在挂载时会先根据根组件创建一个虚拟节点vnode，然后再进行vnode处理
+             */
+            const vnode = createVNode(rootComponet)
+
+            render(vnode, rootContainer)
+        }
+    }
+}
+

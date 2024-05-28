f841cf6e02485f4f01806b2b4cfa4087200f9dc1
Xiang Li <lixiang020317@gmail.com>
2024-05-28 10:47:35 +0800

    Runtime: set rollup module bundler and code packaging

--- example/helloWorld/main.js
+++ example/helloWorld/main.js
@@ -1,1 +1,4 @@
+import { createApp } from "../../lib/mini-vue-implement.esm.js";
+import { App } from "./App.js";
+
 createApp(App).mount("#app");
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export { h } from './runtime-core/h';
+export * from './runtime-core/index';

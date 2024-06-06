63c33d3a620d703db8e99d26bb7bcf99e2fad3fd
Xiang Li <lixiang020317@gmail.com>
2024-06-06 10:47:51 +0800

    Runtime: implement emit function

--- example/helloWorld/Foo.js
+++ example/helloWorld/Foo.js
@@ -1,11 +1,19 @@
 import { h } from "../../lib/mini-vue-implement.esm.js";
 
 export const Foo = {
-  setup(props) {
-    console.log("props: ", props);
-    props.count++;
+  setup(props, { emit }) {
+    // console.log("props: ", props);
+    // props.count++;
+    const emitAdd = () => {
+      emit("countAdd", 3, 4, 5);
+      emit("count-add", 2);
+    };
+    return { emitAdd };
   },
   render() {
-    return h("div", {}, "Foo: " + this.count);
+    console.log("foo", this);
+    const btn = h("button", { onClick: this.emitAdd }, "Click me");
+    const foo = h("p", {}, "Foo: " + this.count);
+    return h("div", {}, [btn, foo]);
   },
 };
---
+++ src/runtime-core/componentEmit.ts
@@ -0,0 +1,10 @@
+import { camelize, toHandlerKey } from "../shared/index";
+
+export function emit(instance, event, ...rawArgs) {
+    const key = toHandlerKey(camelize(event));
+    const { props } = instance;
+    let handler = props[key];
+
+    handler(...rawArgs)
+}
+
